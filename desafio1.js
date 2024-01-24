b95ce21a7cec6a6ce499fa448a166a0cdb21d2d1
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-01-24 14:23:44 -0300

    Create desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,23 @@
+//operadores boleanos
+
+let numeroUm = 1
+let stringUm = '1'
+
+if(numeroUm !== stringUm){
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+
+if (numeroUm === stringUm) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+  } else {
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  }
+
+  if (numeroUm !== stringUm) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
