09ee8ae90da65a0b2728beb28d7e36cf1dd59816
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-01-29 15:50:17 -0300

    DESAFIO 3

--- desafio1.js
+++ desafio1.js
@@ -1,7 +1,12 @@
 //operadores boleanos
+//Olá, aqui minha resolução. 
 
 let numeroUm = 1
 let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
 
 if(numeroUm !== stringUm){
     console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
@@ -10,13 +15,13 @@ if(numeroUm !== stringUm){
 }
 
 
-if (numeroUm === stringUm) {
+if (numeroTrinta === stringTrinta) {
     console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
   } else {
     console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
   }
 
-  if (numeroUm !== stringUm) {
+  if (numeroDez !== stringDez) {
     console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
   } else {
     console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
--- desafio2.js
+++ desafio2.js
@@ -1,8 +1,12 @@
+//Olá, segue minha resolução
+//Variáveis.
+
 const nome = window.prompt('Qual o seu nome');
 const idade = window.prompt('Quantos anos você tem, ' + nome + '?');
 const linguagem = window.prompt('Qual a linguagem de programação você está estudando, ' + nome +'?');
 
-document.getElementById('texto').innerHTML = 'Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + linguagem + '!';
+window.alert ('Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + linguagem + '!')
+
 
 let escolha = window.prompt('Você gosta de estudar' + linguagem + '? Responda com o número 1 para SIM ou 2 para NÂO');
 
